00d51ca1aba923c5b0dc90717b66ea6e2aa6593a
Miroslav <Miroslav10009@gmail.com>
2025-09-07 22:05:42 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "ваш-api-key",
+    authDomain: "ваш-project.firebaseapp.com",
+    projectId: "ваш-project-id",
+    storageBucket: "ваш-project.appspot.com",
+    messagingSenderId: "ваш-sender-id",
+    appId: "ваш-app-id"
+};
